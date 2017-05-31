53a00d83a9c841d7cb6bd4fa64cdb54299b33e54
Dmytro <lizometrix@gmail.com>
2017-05-31 19:00:53 +0300

    Added integer check func; main loop added; README.md modified

--- README.md
+++ README.md
@@ -7,10 +7,8 @@ fdgen [OPTION]...
 	in current path
 
 	-l --	Number of levels of directories
-	-f --	Number of files in each directory
+	-n --	Number of files in each directory
 	-p --	Where to place created structure
-	-t --	Generate full complete binary tree with
-		depth dirDepth and fileCount files in each directory
 	-e --	Set extension for created files (*.tmp by default)
 	-f --	Set file name prefix ("file" by default)
 	-d --	Set directory name prefix ("dir" by default)
--- fdgen.sh
+++ fdgen.sh
@@ -1,31 +1,49 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
-# Default values
+# -e is to exit on first error
 
+######## Default values
 LEVELS=10
-FILESCOUNT=10
-PTH='.'
-TREE='NO'
-FILEEXT='.tmp'
+FCOUNT=10
+PTH=$(pwd)
+FEXT='tmp'
 FNAME='file'
 DNAME='dir'
+########
 
+# Exit with non-zero status if passed parameter is not integer
+# greater than zero
+function isInt() {
+  local target=$1
+
+  if ! [ "$target" -eq "$target" -a "$target" -gt 0 ] 2> /dev/null
+  then
+      echo "$target is not valid integer value" >&2
+      exit 1
+  fi
+}
+
+# Process script's commands
 while [[ $# -gt 1 ]]; do
   key="$1"
-
   case $key in
       -l) LEVELS="$2"
+          isInt $LEVELS
           shift
           ;;
-      -n) FILESCOUNT="$2"
+      -n) FCOUNT="$2"
+          isInt $FCOUNT
           shift
           ;;
       -p) PTH="$2"
+          # Check if PTH is valid path to directory
+          if [ ! -d "$PTH" ]; then
+            echo "$PTH is not a valid path" >&2
+            exit 1
+          fi
           shift
           ;;
-      -t) TREE=YES
-          ;;
-      -e) FILEEXT="$2"
+      -e) FEXT="$2"
           shift
           ;;
       -f) FNAME="$2"
@@ -39,4 +57,19 @@ while [[ $# -gt 1 ]]; do
           ;;
   esac
   shift
+done
+
+# Main logic
+dirCnt=0
+fileCount=0
+foldersPath=''
+
+while [ $dirCnt -lt $LEVELS ]; do
+  foldersPath+="/$DNAME$((++dirCnt))"
+  mkdir "$PTH$foldersPath"
+
+  while [ $fileCount -lt $FCOUNT ]; do
+    touch "$PTH$foldersPath/$FNAME$((++fileCount)).$FEXT"
+  done
+  fileCount=0
 done
